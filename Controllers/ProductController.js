aad3014c0671a68fb79b8bba3000c59b7e78f905
EddyBautista <bautista.j.eddy@gmail.com>
2020-11-05 20:23:41 -0600

    controller to find single id

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -1,5 +1,6 @@
 const Product = require('../Models/ProductModel');
-
+// @desc Gets All Products
+// @route GET /api/products
 async function getProducts(req, res) {
     try { // use await on promise response 
         const products = await Product.findAll()
@@ -12,6 +13,27 @@ async function getProducts(req, res) {
     }
 }
 
+// @desc Gets Single Products
+// @route GET /api/product/:id
+async function getProduct(req, res, id) {
+    try { // use await on promise response 
+        const product = await Product.findById(id)
+        // Checks to see if product exist
+        if(!product){
+            res.writeHead(404, { 'Content-Type': 'application/json'})
+            res.end(JSON.stringify({ message: 'Product Not Found'}))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json'})
+            res.end(JSON.stringify(product))
+            
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    getProducts
+    getProducts,
+    getProduct
 }
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const http = require('http');
-const { getProducts } = require('./Controllers/ProductController')
+// destructuring products
+const { getProducts, getProduct } = require('./Controllers/ProductController')
 
 
 // create server variable 
 const server = http.createServer((req, res) => {
+    // set specific url 
     if (req.url === '/api/products' && req.method === 'GET') {
         // switch from html response to sending json information from the products.json
         getProducts(req, res);
+    }else if(req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET'){
+        const id = req.url.split('/')[3]  // api/products/1  -- sing the split method and grabbing the id at the end of the url to pass into the get product method 
+        getProduct(req, res, id)
     } else {
         // Incorrect url
         res.writeHead(404, {
-            'Content-Type': 'application/json'C
+            'Content-Type': 'application/json'
         })
         res.end(JSON.stringify({
             message: 'Route Not Found'
